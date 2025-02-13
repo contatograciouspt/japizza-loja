41c27d0e51be256c1e51c2f38401f47f8b4dd82d
contatograciouspt <contato@gracious.pt>
2025-02-13 19:36:42 -0300

    intagração com zonesoft status: homologação 50

--- src/services/CustomerServices.js
+++ src/services/CustomerServices.js
@@ -36,8 +36,8 @@ const CustomerServices = {
     return requests.post("/customer/change-password", body);
   },
 
-  updateCustomer: async (email, body) => {
-    return requests.put(`/customer/${email}`, body);
+  updateCustomer: async (id, body) => {
+    return requests.put(`/customer/${id}`, body);
   },
 
   getShippingAddress: async ({ userId = "" }) => {
